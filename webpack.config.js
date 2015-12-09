ede0e031910829cc01766663b34ba275b7a61a50
清水俊郎 <toshilow@gramp.biz>
2015-12-09 16:08:48 +0900

    Add source from vuejs/vue-webpack-example

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,14 @@
-
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: "./src/main.js",
   output: {
-    path: __dirname,
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['', '.js']
+    path: "./build",
+    publicPath: "/build/",
+    filename: "build.js"
   },
   module: {
-      loaders: [
-        {
-          test: /\.vue$/,
-          loader: 'vue'
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015']
-          }
-        }
-      ],
-   }
+    loaders: [
+      { test: /\.styl$/, loader: "style!css!stylus" },
+      { test: /\.html$/, loader: "html" }
+    ]
+  }
 }
